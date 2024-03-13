59ad85a2764c2991c067f7fb332f6c6cc8a96e2d
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-03-13 23:58:35 +0530

    Time: 4 ms (35.26%), Space: 7.2 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,49 +1,21 @@
-// class Solution {
-// public:
-//     int pivotInteger(int n) 
-//     {
-//         int sum1=0;
-//         int sum2=0;
-
-//         for(int i=1;i<=n;i++)
-//         {
-//            int pivot=i;
-//            sum1=(i)*(i+1)/2;
-//            for(int j=i;j<=n;j++){
-//             sum2=sum2+j;
-//            }
-
-//            if(sum1==sum2){
-//             return pivot;
-//            }
-//         }
-
-
-
-//         return -1;
-        
-//     }
-// };
-
-
 class Solution {
 public:
-    // Method to find the pivot integer for a given n
+   
     int pivotInteger(int n) {
-        // Calculate the sum of all integers from 1 to n
-        int sum = n * (n + 1) / 2;
-      
-        // Find the integer square root of the sum, if it exists
-        int squareRoot = static_cast<int>(sqrt(sum));
-      
-        // Check if the square of the square root equals the original sum
-        // If so, the pivot integer exists and is equal to squareRoot
-        if (squareRoot * squareRoot == sum) {
-            return squareRoot;
+        for(int i=1;i<=n;i++){
+            int leftsum = (i * (i + 1) / 2);
+            int rightsum = (n * (n + 1) / 2)-(i*(i-1))/2;
+            
+            if(leftsum==rightsum) return i;
+            
         }
-        // If no such pivot integer exists, return -1
+        
+      
+       
         return -1;
     }
 };
+
+
 
 
