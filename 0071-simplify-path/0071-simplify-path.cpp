5c09bb27369a670e3319fafb0d943158fc6c251d
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-05-11 20:38:26 +0530

    Time: 4 ms (73.10%), Space: 11.9 MB (18.91%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,53 @@
+#include <stack>
+#include <string>
+
+class Solution {
+public:
+    void buildAns(std::stack<std::string>& st, std::string& ans) {
+        if (st.empty()) {
+            return;
+        }
+
+        std::string minPath = st.top();
+        st.pop();
+        buildAns(st, ans);
+        ans += minPath; // Concatenate path components without "/"
+    }
+
+    string simplifyPath(string path) {
+        stack<string> st;
+        int i = 0;
+        while (i < path.size()) {
+            int start = i;
+            int end = i + 1;
+
+            // Find next directory name
+            while (end < path.size() && path[end] != '/') {
+                end++;
+            }
+
+            // Extract the directory name
+            string minPath = path.substr(start, end - start);
+
+            i = end; // Move to the next position in the path
+
+            // Handle directory names
+            if (minPath != "/" && minPath != "/." && minPath != "/..") {
+                st.push(minPath);
+            } else if (minPath == "/.." && !st.empty()) {
+                st.pop(); // Move one level up
+            }
+        }
+
+        // Construct the simplified path
+        string ans;
+        if (st.empty()) 
+        {
+            ans = "/";
+        } else {
+            buildAns(st, ans);
+        }
+
+        return ans;
+    }
+};
