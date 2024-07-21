6b28e5ae5689e3a1eb975f39c533edc7f50e3114
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-07-21 14:15:30 +0530

    Time: 20 ms (36.10%), Space: 21.4 MB (17.18%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,39 +1,45 @@
 class MinStack {
 public:
-    vector<pair<int,int>>st;
-    
+        
+        vector<pair<int ,int>>st;
+        
     MinStack() {
         
     }
     
     void push(int val) {
-        if(st.empty())
-        {
-            pair<int,int>p=make_pair(val,val);
-            st.push_back(p);
-        }
-        else{
-            pair<int,int>p;
-            p.first=val;
-            p.second=min(val,st.back().second);
-            st.push_back(p);
-        }
+            
+            if(st.empty()){
+                    pair<int,int>p=make_pair(val,val);
+                    st.push_back(p);
+            }
+            
+            else{
+                    pair<int,int>p;
+                    p.first=val;
+                    p.second=min(val,st.back().second);
+                    st.push_back(p);
+            }
+            
+            
+            
         
     }
     
     void pop() {
-        st.pop_back();
+            
+            st.pop_back();
         
     }
     
     int top() {
-        return st.back().first;
+            
+            return st.back().first;
         
     }
     
-    int getMin() 
-    {
-        return st.back().second;
+    int getMin() {
+            return st.back().second;
         
     }
 };
