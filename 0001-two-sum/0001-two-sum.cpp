5d0b598d6eac96113b2efed4bc4cffd44332b3e0
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-08-02 09:02:18 +0530

    Time: 10 ms (65.41%), Space: 14.5 MB (16.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,32 +1,45 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+// int prev,next;
+
+//         for(int i=0;i<nums.size();i++)
+//         {
+//              for(int j=i+1;j<nums.size();j++){
+
+             
+//                 if(nums[i]+nums[j]==target)
+//                 {
+//                     prev=i;
+//                     next=j;
+//                     break;
+//                 }
+//              }
+//         }
+//         return {prev,next};
+        
+        
+//     }
+// };
+
+
+// Better Solution
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
-    {
-        
-        unordered_map<int,int>m;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(m.find(target-nums[i])!=m.end())
-            {
-                return {m[target-nums[i]],i};
-                
-            }
+    vector<int> twoSum(vector<int>& nums, int target) {
+int prev,next;
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int curr=nums[i];
+            int wefind=target-curr;
             
-            else
-            
-            {
-                m[nums[i]]=i;
-            }
-            
-            
-        }
-        
-        
-        return {-1,-1};
-        
+
+            if(mp.find(wefind)!=mp.end()){
+                return {i,mp[wefind]};
+               }
+                mp[curr]=i;
+                 }
+                 return {-1,-1};
     }
 };
-
-
-
-
