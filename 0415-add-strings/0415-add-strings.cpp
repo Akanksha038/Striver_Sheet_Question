6bbf2373606202c0a9f0e292291fddb263d42bb1
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-08-10 21:25:24 +0530

    Time: 5 ms (50.81%), Space: 8.5 MB (51.72%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int carry = 0;  // Initialize carry to 0
-        string result = "";  // Initialize result as an empty string
-        int i = num1.size() - 1;
-        int j = num2.size() - 1;
-
-        // Process each digit from the end of both strings
-        while (i >= 0 || j >= 0 || carry != 0) {
-            int sum = carry;
             
-            // Add digit from num1 if available
-            if (i >= 0) {
-                sum += num1[i] - '0';
-                i--;
+            
+            int carry=0;
+            string result="";
+            int i= num1.size()-1; //1
+            
+            int j= num2.size()-1; //2
+            
+            
+           
+            while(i>=0 || j>=0||carry!=0){
+                    
+                    
+                    int sum=carry;
+                    if( i>=0){ sum=sum+num1[i]-'0'; 
+                    i--;
+                             }
+                    
+                    if( j>=0){ sum=sum+num2[j]-'0';
+                    
+                    j--;
+                             }
+                    // sum=sum+carry;
+                    carry=sum/10;
+                    sum=sum%10;
+                    
+                    result.push_back(sum+'0');  //2. Conversion from Integer to Character (+'0'):
+                    
+                    
+                    
             }
             
-            // Add digit from num2 if available
-            if (j >= 0) {
-                sum += num2[j] - '0';
-                j--;
-            }
+            reverse(result.begin(), result.end());
+            return result;
+ 
             
-            // Compute new carry and digit to be added to result
-            carry = sum / 10;
-            result.push_back((sum % 10) + '0');
-        }
-
-        // Reverse the result to get the correct order
-        reverse(result.begin(), result.end());
-        return result;
+            
+            
     }
 };
