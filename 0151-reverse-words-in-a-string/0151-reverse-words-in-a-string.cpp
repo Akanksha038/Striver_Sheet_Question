c7896e122f04b5541d81951f988e1913144e1cb3
Akanksha Gupta <114081059+Akanksha038@users.noreply.github.com>
2024-09-22 14:43:36 +0530

    Time: 3 ms (81.24%), Space: 23.5 MB (15.06%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    string reverseWords(string s) 
-    {
-        string helper, res;
-        reverse(s.begin(), s.end());  // Reverse the entire string
-
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (s[i] != ' ') 
+    string reverseWords(string s) {
+            
+            string ans="";
+            int n=s.size();
+            int i=0;
+            while(i<n)
             {
-                helper += s[i];  // Build the current word
+                    string temp="";
+                    
+                    while(s[i]==' '&& i<n){
+                            i++;
+                    }
+                    
+                    while(s[i]!=' ' && i<n){
+                            temp+=s[i];
+                            i++;
+                    }
+                    
+                    if(temp.size()>0){
+                            if(ans.size()==0){
+                                    ans=temp;
+                            }
+                            
+                            else{
+                                    ans=temp+" "+ans;
+                            }
+                    }
             }
-            else if (!helper.empty())
-            {
-                reverse(helper.begin(), helper.end());  // Reverse the current word
-                if (!res.empty()) res += " ";  // Add a space before the next word if the result is not empty
-                res += helper;  // Add the reversed word to the result
-                helper.clear();  // Clear the helper for the next word
-            }
-        }
-
-        // Add the last word (if any)
-        if (!helper.empty()) 
-        {
-            reverse(helper.begin(), helper.end());
-            if (!res.empty()) res += " ";
-            res += helper;
-        }
-
-        return res;
+            
+            return ans;
+        
     }
 };
